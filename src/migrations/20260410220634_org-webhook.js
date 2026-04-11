73ee408b9fcb1ef91150f71fbf11f0e79417d2bb
namanmahna2 <namanmahna2@gmail.com>
2026-04-11 23:14:02 +0100

    migration - org-webhook table

---
+++ src/migrations/20260410220634_org-webhook.js
@@ -0,0 +1,34 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const table_ = "org-webhook"
+exports.up = async function (knex) {
+    await knex.schema.createTable(table_, (table) => {
+        table.increments("id").primary();
+        table.integer("github_account_id")
+            .references("id")
+            .inTable("github_accounts")
+            .onDelete("CASCADE");
+
+        table.string("org_login").notNullable();
+
+        table.bigInteger("github_hook_id").notNullable();
+
+        table.string("webhook_url").notNullable();
+
+        table.boolean("is_active").defaultTo(true);
+
+        table.timestamps(true, true);
+
+        table.unique(["org_login", "github_hook_id"]);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    await knex.schema.dropTableIfExists(table_)
+};
